022a3687eb75a97aaacfffe1b85d0a9a102d08d1
MatZxZx <quinteros.matias.et21.21@gmail.com>
2024-04-18 15:48:32 -0300

    Login funcional

---
+++ src/components/Form.jsx
@@ -0,0 +1,55 @@
+import "./Form.css"
+import { useState } from "react"
+
+export function Form(){
+
+    const [name, setName] = useState("")
+    const [pass, setPass] = useState("")
+    const [error, setError] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(name == "" || pass == ""){
+
+            setError(true)
+            return
+
+        }
+
+        setError(false)
+
+    }
+
+    return(
+
+        <section>
+
+            <h1>Login</h1>
+
+            <form className="form"
+            onSubmit={handleSubmit}
+            >
+
+
+                <input type="text" 
+                
+                value={name} onChange={e => setName(e.target.value)}
+
+                />
+                <input type="password" 
+                
+                value={pass} onChange={e => setPass(e.target.value)}
+
+                />
+                <button>Iniciar Sesión</button>
+
+            </form>
+
+            {error && <p>Complete todos los campos e intente de nuevo.</p>}
+        
+        </section>
+
+    )
+
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+export function Home() {
+
+    return(
+
+        <div>
+            <h1>HOLA NEGROS</h1>
+        </div>
+
+    )
+
+}
