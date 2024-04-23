f4da92a7d54eb71787af96afa22548771b4bee4b
Lautaro Chaparro <lautaro.chaparro.et21.21@gmail.com>
2024-04-23 11:17:48 -0300

    mati pushea porfavor

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
 import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from './ProtectedRoute'
 import Landing from './pages/Landing/Landing'
-import Register from './pages/Register/Register'
 import Home from './pages/Home/Home'
 import Profile from './pages/Profile/Profile'
+import Register from './pages/Register/Register'
 import Welcome from './pages/Welcome/Welcome'
 import Team from './pages/Team/Team'
-import { useSelector } from 'react-redux'
-import useAuth from './hooks/useAuth'
 
-function App() {
-
-  const auth = useSelector(state => state.auth)
-  const { setAuth, setLoading } = useAuth()
-
-  const { isAuth, isLoading } = auth.auth
-
+function DevApp() {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/register' element={<Register />} />
-        <Route element={<ProtectedRoute/>} >
-          <Route path='/home' element={<Home />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/welcome' element={<Welcome />} />
-          <Route path='/team' element={<Team />} />
-        </Route>
-      </Routes>
-      {/* <h1 className='text-white text-2xl'>{`${isAuth} ${isLoading}`}</h1>
-      <button className='bg-white' onClick={() => setAuth(!isAuth)}>
-        autorizar
-      </button>
-      <button className='bg-white' onClick={() => setLoading(!isLoading)}>
-        cargando
-      </button> */}
-    </>
-    
+    <Routes>
+      <Route path='mati'></Route>
+      <Route path='facu'></Route>
+      <Route path='chap'></Route>
+    </Routes>
   )
 }
 
-export default App
+function App() {
+  return (
+    // Paginas
+    <Routes>
+      {/* Rutas Globales */}
+      <Route path='/' element={<Landing />} />
+      <Route path='/register' element={<Register />} />
+      {/* Rutas Protegidas */}
+      <Route element={<ProtectedRoute/>} >
+        <Route path='/home' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/welcome' element={<Welcome />} />
+        <Route path='/team' element={<Team />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default DevApp
